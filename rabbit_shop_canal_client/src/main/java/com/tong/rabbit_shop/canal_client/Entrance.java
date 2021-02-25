dd2dc2e5eb274fe470d4baf1fc2c32de263ff759
z15700400@163.com <00400_glo@zkt.com>
2021-02-25 10:49:57 +0800

    Initial commit

---
+++ rabbit_shop_canal_client/src/main/java/com/tong/rabbit_shop/canal_client/Entrance.java
@@ -0,0 +1,8 @@
+package com.tong.rabbit_shop.canal_client;
+
+public class Entrance {
+    public static void main(String[] args) {
+        CanalClient canalClient = new CanalClient();
+        canalClient.start();
+    }
+}
